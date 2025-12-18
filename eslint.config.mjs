a25ef3ab950a8764668ba96c7195d48f4f836f4a
rebeccahongsf <rebeccahongsf@gmail.com>
2025-12-18 15:12:43 -0800

    SWSWEB-33 SWSWEB-30 | install dependencies and update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
-import nextTs from 'eslint-config-next/typescript';
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,89 +8,89 @@ const eslintConfig = defineConfig([
   // Global ignores from .eslintignore
   globalIgnores([
     // Default Next.js ignores
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
     // Custom ignores
-    'node_modules/**',
-    'tailwind/**',
-    'postcss.config.js',
-    '*.min.js',
-    '*.test.js',
+    "node_modules/**",
+    "tailwind/**",
+    "postcss.config.js",
+    "*.min.js",
+    "*.test.js",
   ]),
   // Custom rules from .eslintrc.json
   {
     rules: {
-      'comma-dangle': ['error', {
-        objects: 'always-multiline',
-        arrays: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
+      "comma-dangle": ["error", {
+        objects: "always-multiline",
+        arrays: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "always-multiline",
       }],
-      'max-len': ['warn', { code: 120, ignoreStrings: true }],
-      'no-param-reassign': 'off',
-      'no-redeclare': 'off',
-      'no-undef': 'off',
-      'no-underscore-dangle': 'off', // We have _uid from Storyblok that can't be changed
-      'object-curly-newline': ['error', { consistent: true, minProperties: 4 }],
-      'import/extensions': 'off',
-      'no-shadow': 'off',
-      'jsx-a11y/label-has-associated-control': [2, {
-        components: ['Label'],
+      "max-len": ["warn", { code: 120, ignoreStrings: true }],
+      "no-param-reassign": "off",
+      "no-redeclare": "off",
+      "no-undef": "off",
+      "no-underscore-dangle": "off", // We have _uid from Storyblok that can't be changed
+      "object-curly-newline": ["error", { consistent: true, minProperties: 4 }],
+      "import/extensions": "off",
+      "no-shadow": "off",
+      "jsx-a11y/label-has-associated-control": [2, {
+        components: ["Label"],
         required: {
-          some: ['nesting', 'id'],
+          some: ["nesting", "id"],
         },
       }],
-      'jsx-quotes': ['error', 'prefer-double'],
-      quotes: [2, 'double',
+      "jsx-quotes": ["error", "prefer-double"],
+      quotes: [2, "double",
         { avoidEscape: true, allowTemplateLiterals: true },
       ],
-      'space-before-function-paren': 0,
-      'react/jsx-props-no-spreading': 0,
-      'react/prop-types': 0,
-      'react/jsx-handler-names': 0,
-      'react/jsx-filename-extension': [
-        'error',
+      "space-before-function-paren": 0,
+      "react/jsx-props-no-spreading": 0,
+      "react/prop-types": 0,
+      "react/jsx-handler-names": 0,
+      "react/jsx-filename-extension": [
+        "error",
         {
           extensions: [
-            '.js',
-            '.ts',
-            '.jsx',
-            '.tsx',
+            ".js",
+            ".ts",
+            ".jsx",
+            ".tsx",
           ],
         },
       ],
-      'react/jsx-fragments': 0,
-      'react/jsx-one-expression-per-line': 0,
-      'react/no-unused-prop-types': 0,
-      'react/require-default-props': 0,
-      'react/jsx-no-useless-fragment': [2, { allowExpressions: true }], // https://github.com/jsx-eslint/eslint-plugin-react/issues/2584
-      'react/jsx-indent': [2, 2],
-      'react/display-name': 0,
-      'class-methods-use-this': 0,
-      'import/prefer-default-export': 0,
-      'import/no-extraneous-dependencies': 0,
-      'import/no-cycle': 0,
-      'import/no-unresolved': 0,
-      'no-unused-vars': 0,
-      'no-multi-spaces': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
+      "react/jsx-fragments": 0,
+      "react/jsx-one-expression-per-line": 0,
+      "react/no-unused-prop-types": 0,
+      "react/require-default-props": 0,
+      "react/jsx-no-useless-fragment": [2, { allowExpressions: true }], // https://github.com/jsx-eslint/eslint-plugin-react/issues/2584
+      "react/jsx-indent": [2, 2],
+      "react/display-name": 0,
+      "class-methods-use-this": 0,
+      "import/prefer-default-export": 0,
+      "import/no-extraneous-dependencies": 0,
+      "import/no-cycle": 0,
+      "import/no-unresolved": 0,
+      "no-unused-vars": 0,
+      "no-multi-spaces": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
         {
-          vars: 'local',
-          args: 'none',
+          vars: "local",
+          args: "none",
         },
       ],
-      'import/export': 0,
-      'func-names': 0,
-      semi: [1, 'always'], // 1 is for warning
-      '@next/next/no-img-element': 0,
+      "import/export": 0,
+      "func-names": 0,
+      semi: [1, "always"], // 1 is for warning
+      "@next/next/no-img-element": 0,
     },
     settings: {
       tailwindcss: {
-        callees: ['classnames', 'clsx', 'ctl', 'cva', 'tv', 'cn', 'dcnb', 'cnb'], // Tailwind utility class detection for specific libraries
+        callees: ["classnames", "clsx", "ctl", "cva", "tv", "cn", "dcnb", "cnb"], // Tailwind utility class detection for specific libraries
       },
     },
   },
